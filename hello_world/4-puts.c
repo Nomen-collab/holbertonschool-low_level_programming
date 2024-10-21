919818ef264d894f28c565676bbf8087f62e01c5
Nomen-collab <nomenaratsimba01@gmail.com>
2024-10-21 12:45:27 +0000

    exo4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo '#include <stdio.h> int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }' > puzzle.c && gcc -Wall -Werror -Wextra -pedantic -std=gnu89 puzzle.c -o puzzle && ./puzzle
-
+#include <stdio.h> int main(void) { puts("programming is like building a multilingual puzzle"); return 0; }
