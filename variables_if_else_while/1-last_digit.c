d3f7c58b4050d28236bfa736ac91cb4e55c85ed5
Nomen-collab <nomenaratsimba01@gmail.com>
2024-10-23 10:35:32 +0000

    exo1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,33 +2,35 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - Entry point
-* Description: Generates random number and prints the last digit
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ * Description: Generates random number and prints the last digit
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n;
-int last_digit;
+	int n;
+	int last_digit;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-last_digit = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
-/* Print the last digit and the corresponding message */
-printf("Last digit of %d is %d and is ", n, last_digit);
+	/* Print the last digit and the corresponding message */
+	printf("Last digit of %d is %d and is ", n, last_digit);
 
-if (last_digit > 5)
-{
-printf("greater than 5\n");
+	if (last_digit > 5)
+	{
+		printf("greater than 5\n");
+	}
+	else if (last_digit == 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("less than 6 and not 0\n");
+	}
+
+	return (0);
 }
-else if (last_digit == 0)
-{
-printf("0\n");
-}
-else
-{
-printf("less than 6 and not 0\n");
-}
 
-return (0);
