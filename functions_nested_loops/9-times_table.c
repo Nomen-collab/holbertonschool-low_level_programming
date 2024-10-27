0a21245f7cf1c5a9311558b593f234ba0ae9c438
Nomen-collab <nomenaratsimba01@gmail.com>
2024-10-27 21:28:53 +0000

    exo9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- *	times_table - imprime la table de multiplication par 9
+ *	times_table - imprime la table de multiplication de 0 à 9
  */
-void	times_table(void)
+void times_table(void)
 {
-	int	i;
-	int	j;
-	int	product;
+	int i, j, product;
 
 	for (i = 0; i <= 9; i++)
 	{
@@ -27,9 +25,13 @@ void	times_table(void)
 				if (product < 10)
 				{
 					_putchar(' ');
+					_putchar(product + '0');
 				}
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
 			}
 		}
 		_putchar('\n');
