e218e30a030949c6fb4f66767ac7263fe96751e4
Nomen-collab <nomenaratsimba01@gmail.com>
2024-10-30 13:03:43 +0000

    exo6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Dessine une ligne dans le terminal.
+ * @n: Le nombre de fois ou le caractere '_' doit etre imprime.
+ *
+ * Return: rien.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
