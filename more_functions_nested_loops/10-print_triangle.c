c4bf1bc11adc4f37586f02b6dfe15d857489a337
Nomen-collab <nomenaratsimba01@gmail.com>
2024-10-30 15:06:11 +0000

    exo10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Affiche un triangle suivi d'une nouvelle ligne.
+ * @size: taille du triangle.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = size - i; j > 0; j--)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
