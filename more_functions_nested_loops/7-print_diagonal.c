cd10f7ea61db90245fd15cfb1e49c65092c0d827
Nomen-collab <nomenaratsimba01@gmail.com>
2024-10-30 14:08:39 +0000

    exo7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,28 @@
  */
 void print_diagonal(int n)
 {
-    int i, j;
+	int i = 0;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < i; j++)
-            {
-                _putchar(' ');
-            }
-            _putchar('\\');
-            _putchar('\n');
-        }
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			int spaces = 0;
+
+			while (spaces < i)
+			{
+				_putchar('	');
+				spaces++;
+			}
+
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
 }
 
