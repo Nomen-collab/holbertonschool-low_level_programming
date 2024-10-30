7b2b7fe7e51a436c1a2d13f119c3a7e88af218f3
Nomen-collab <nomenaratsimba01@gmail.com>
2024-10-30 12:31:02 +0000

    exo4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers: affiche les chiffres de 0 a 9 sauf 2 et 4.
+ *
+ * Return : Rien.
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	char numbers[] = "01356789";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(numbers[i]);
+	}
+	_putchar('\n");
+}
