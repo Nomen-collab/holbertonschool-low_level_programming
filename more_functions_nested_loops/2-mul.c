c0a43687be628af7df4792c896b3be59fe870c66
Nomen-collab <nomenaratsimba01@gmail.com>
2024-10-30 12:08:27 +0000

    exo2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Multiplie deux entiers
+ * @a: le premier entier
+ * @b: le deuxieme entier
+ *
+ * Return: le produit de a et b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
