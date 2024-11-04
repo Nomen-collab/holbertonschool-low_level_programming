5b8b3c82f872bf4c0ae59c73af292ca15ad81b8d
Nomen-collab <nomenaratsimba01@gmail.com>
2024-11-04 10:53:29 +0000

    exo6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * puts2 - Affiche un caractère sur deux d'une chaî
+ * puts2 - Affiche un caractère sur deux d'une cha�ne
  * @str: Pointeur vers la chaîne de caractèr a afficher
  */
-void	puts2(char *str)
+void puts2(char *str)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (str[i] != '\0')
