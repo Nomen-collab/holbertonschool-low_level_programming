a161e55fc7ea9f1380537c0533e44f40f910f0bb
Nomen-collab <nomenaratsimba01@gmail.com>
2024-11-04 10:13:44 +0000

    exo1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - change les valeurs de deux entiers
+ * @a: Pointeur vers le premier entier
+ * @b: Pointeur vers le deuxième entier
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
