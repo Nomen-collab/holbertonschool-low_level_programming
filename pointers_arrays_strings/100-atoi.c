bf37dcb20b963cc02c01f214e9d659b0fbb67c4a
Nomen-collab <nomenaratsimba01@gmail.com>
2024-11-04 14:21:23 +0000

    exo100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - Convertit une chaîne de caractères en entier
@@ -8,11 +9,7 @@
  */
 int _atoi(char *s)
 {
-	int i, sign, result;
-
-	i = 0;
-	sign = 1;
-	result = 0;
+	int i = 0, sign = 1, result = 0;
 
 	while (s[i] != '\0')
 	{
@@ -31,6 +28,11 @@ int _atoi(char *s)
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			if (result > (INT_MAX / 10) ||
+			    (result == (INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10)))
+			{
+				return (sign == 1 ? INT_MAX : INT_MIN);
+			}
 			result = result * 10 + (s[i] - '0');
 		}
 		else if (result > 0)
