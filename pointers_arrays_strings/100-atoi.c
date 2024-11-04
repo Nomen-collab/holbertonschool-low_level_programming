3b1f70b8f43d14b91e6b123dd65af2a8a72881cf
Nomen-collab <nomenaratsimba01@gmail.com>
2024-11-04 14:03:57 +0000

    exo100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,11 +8,12 @@
  */
 int _atoi(char *s)
 {
-	int i, sign, result;
+	int i, sign, result, num_started;
 
 	i = 0;
 	sign = 1;
 	result = 0;
+	num_started = 0;
 
 	while (s[i] != '\0')
 	{
@@ -26,15 +27,16 @@ int _atoi(char *s)
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			result = result * 10 + (s[i] - '0') * sign;
+			num_started = 1;
+			result = result * 10 + (s[i] - '0');
 			if (sign == -1 && result > 0)
 			{
-				return (0);
+				result = -result;
 			}
-			if (sign == 1 && result < 0)
-			{
-				return (0);
-			}
+		}
+		else if (num_started)
+		{
+			break;
 		}
 		i++;
 	}
