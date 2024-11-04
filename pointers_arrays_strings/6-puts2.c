7e5e669bc41c6097d9ac00383b91b15ff0eecbff
Nomen-collab <nomenaratsimba01@gmail.com>
2024-11-04 10:51:22 +0000

    exo6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - Affiche un caractère sur deux d'une chaî
+ * @str: Pointeur vers la chaîne de caractèr a afficher
+ */
+void	puts2(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i])
+
