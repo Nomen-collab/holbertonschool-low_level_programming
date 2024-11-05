288b39d24e250c0e26a3e3b59057967bbca5f01b
Nomen-collab <nomenaratsimba01@gmail.com>
2024-11-05 20:51:16 +0000

    exo7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - Encode une chaîne en utilisant le langage 1337
+ * @str: Pointeur vers la chaîne à encoder
+ *
+ * Return: Pointeur vers la chaîne encodée
+ */
+char *leet(char *str)
+{
+	int i = 0;
+	char map[] = "aAeEoOtTlL";
+	char replace[] = "4433007711";
+
+	while (str[i] != '\0')
+	{
+		for (int j = 0; map[j] != '\0'; j++)
+		{
+			if (str[i] == map[j])
+			{
+				str[i] = replace[j];
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -30,6 +30,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_tring(char *str);
+char *leet(char *str);
 
 #endif /* MAIN_H */
 
