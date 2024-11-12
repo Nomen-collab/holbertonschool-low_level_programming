c54fb918686db04b6318ef1bbcc533ea51c3a58e
Nomen-collab <nomenaratsimba01@gmail.com>
2024-11-12 21:02:23 +0000

    exo2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string
- * @s: The string to search
- * @c: The character to locate
- *
- * Return: Pointer to the first occurrence of c in s, or NULL if not found
+ * _strchr - locate a character in a string
+ * @s: string to search
+ * @c: character to locate
+ * Return: pointer to the first occurrence of c or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
@@ -13,9 +12,9 @@ char *_strchr(char *s, char c)
 	{
 		if (*s == c)
 			return (s);
-		s++;
+		++s;
 	}
-	if (*s == c)
+	if (c == '\0')
 		return (s);
 	return (NULL);
 }
