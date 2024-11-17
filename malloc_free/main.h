0bb6784f0e584f99802694b23b2a8a7f1f8601f3
Nomen-collab <nomenaratsimba01@gmail.com>
2024-11-17 10:17:10 +0000

    exo3

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -41,6 +41,7 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif /* MAIN_H */
 
