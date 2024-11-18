61aaaa87af9ab012480ca26e9d001c0bcee56930
Nomen-collab <nomenaratsimba01@gmail.com>
2024-11-18 09:46:56 +0000

    exo5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
 /**
- * _strstr - locates a substring
- * @haystack: string to search within
- * @needle: substring to locate
- * Return: pointer to the beginning of the located substring
+ * _strstr - finds the first occurrence of the substring needle in haystack
+ * @haystack: string where to look
+ * @needle: string to find
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *h, *n;
+	int l, l2, i, j, c;
 
-	if (*needle == '\0')
-		return (haystack);
-
-	while (*haystack)
+	l = 0, l2 = 0, c = 0;
+	while (haystack[l] != '\0')
+		l++;
+	while (needle[l2] != '\0')
+		l2++;
+	for (i = 0; i < l; i++, haystack++)
 	{
-		h = haystack;
-		n = needle;
-
-		while (*h && *n && (*h == *n))
+		for (j = 0, c = 0; j < l2; j++)
 		{
-			h++;
-			n++;
+			if (haystack[j] == 0)
+				break;
+			if (haystack[j] == needle[j])
+			{
+				c += 1;
+			}
 		}
-
-		if (*n == '\0')
+		if (c == l2)
 			return (haystack);
-
-		haystack++;
 	}
-
-	return (NULL);
+	return (0);
 }
-
